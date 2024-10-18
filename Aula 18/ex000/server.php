e1196fa553fa77c73365dc7f2a30aca49318a520
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-18 16:30:52 -0400

    por hoje é só

---
+++ Aula 18/ex000/server.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" contant="width=device-width, initial-scale=1.0">
+	<title>Testando scripts PHP</title>
+</head>
+<body>
+	<h1>Testando PHP</h1>
+	<?php 
+        $nome = "Gustavo Guanabara";
+        echo "O meu nome é $nome";
+    ?>
+</body>
+</html>
