fe42f7c6eec4b4149da284b78bd482161447c0c7
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-19 20:11:47 -0400

    aula 18 finalizada

--- Aula 18/ex001/server.php
+++ Aula 18/ex001/server.php
@@ -8,8 +8,9 @@
 <body>
     <h1>Exemplo de PHP</h1>
     <?php 
+        date_default_timezone_set("America/Sao_Paulo"); // GMT - 3
         echo "Hoje é dia " . date("d/M/Y");
-        echo "e a hora"
+        echo " e a hora atual é " . date("G:i:s");
     ?>
 </body>
 </html>
