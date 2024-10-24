f6d6065328d529a0739b3a49d42f4fedae343a1d
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-24 14:30:33 -0400

    finalizado o desafio 01

--- Desafios/d001/server.php
+++ Desafios/d001/server.php
@@ -10,8 +10,15 @@
     <div class="container">
         <h1>Resultado Final</h1>
         <?php 
+            $n = $_GET["numero"];
+            $ant = $n - 1;
+            $suc = $n + 1;
             
+            echo "<p>O número escolhido foi <strong>$n</strong></p>";
+            echo "<p>O seu antecessor é <strong>$ant</strong></p>";
+            echo "<p>O seu sucessor é <strong>$suc</strong></p>";
         ?>
+        <form action="javascript:history.go(-1)"><button type="submit">Voltar</button></form>
     </div>
 </body>
 </html>
